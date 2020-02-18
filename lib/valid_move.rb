4941f8505fb52a9f1779d360ab905425a92cad28
Tracy Collins <tc@threeceemedia.com>
2020-02-18 00:56:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,4 +9,10 @@ def valid_move? (index)
 end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, index)
+  if (board[index] == "X" || board[index] == "O")
+    true
+  else
+    false
+  end
+end
